18bb3c5717f2608efdaafc74da54831afd6bd3e5
Rahul Kumar Yadav <yadavrahulkumar91@gmail.com>
2024-08-16 03:58:49 +0545

    First commit

---
+++ app/components/note.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { elseFunction } from '../render'
+
+function Note ({ key, value, isArray, isObject, level }) {
+  return (
+    <div>
+        {value && Array.isArray(value) && value.length > 0
+          ? value.map((note, index) => (
+              <div
+                className='border-r-2 border-black border-2'
+                key={index} // Ensure unique key for each item
+              >
+                <div className='bg-red-500 text-2xl'>Note:</div>
+                <div className='bg-yellow-300 font-sans'>
+    
+                  {elseFunction('', note.data, isArray, isObject, 1)}
+                </div>
+              </div>
+             ))
+          : null}
+      </div>
+  )
+}
+
+export default Note
+
